e8068bd1eba18ee828b9b1a21c57546ceac8405c
Stephen Oba <obastepheno@gmail.com>
2022-05-31 17:05:05 +0100

    updated artist and venue serializer

--- app.py
+++ app.py
@@ -177,8 +177,8 @@ def venue_serializer(venue):
         "seeking_talent": venue.seeking_talent,
         "seeking_description": venue.seeking_description,
         "image_link": venue.image_link,
-        'past_shows': past_shows_query.all(),
-        'upcoming_shows': upcoming_shows_query.all(),
+        'past_shows': [show_serializer(show, Venue) for show in past_shows_query.all()],
+        'upcoming_shows': [show_serializer(show, Venue) for show in upcoming_shows_query.all()],
         "past_shows_count": past_shows_query.count(),
         "upcoming_shows_count": upcoming_shows_query.count()
     }
@@ -243,7 +243,7 @@ def search_venues():
 def show_venue(venue_id):
     # shows the venue page with the given venue_id
     # TODO: add remaining data for show
-    data = venue_serializer(Venue.query.filter(Venue.id == venue_id).first())
+    data = venue_serializer(Venue.query.get_or_404(venue_id))
     data1 = {
       "id": 1,
       "name": "The Musical Hop",
@@ -354,7 +354,6 @@ def create_venue_submission():
         print(e)
     finally:
         db.session.close()
-        print(error)
         if not error:
             flash(
                 f'Venue {form.name.data} was successfully listed!',
@@ -420,11 +419,36 @@ def search_artists():
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
+def artist_serializer(artist):
+    shows_query = db.session.query(Show).select_from(
+        Artist).join(Show, Artist.id == Show.artist_id).filter(Show.artist_id == artist.id)
+    past_shows_query = shows_query.filter(Show.start_time < datetime.now())
+    upcoming_shows_query = shows_query.filter(Show.start_time >= datetime.now())
+
+    data = {
+        'id': artist.id,
+        'name': artist.name,
+        "genres": artist.genres,
+        "city": artist.city,
+        "state": artist.state,
+        "phone": artist.phone,
+        "website": artist.website_link,
+        "facebook_link": artist.facebook_link,
+        "seeking_venue": artist.seeking_venue,
+        "seeking_description": artist.seeking_description,
+        "image_link": artist.image_link,
+        'past_shows': [show_serializer(show, Artist) for show in past_shows_query.all()],
+        'upcoming_shows': [show_serializer(show, Artist) for show in upcoming_shows_query.all()],
+        "past_shows_count": past_shows_query.count(),
+        "upcoming_shows_count": upcoming_shows_query.count()
+    }
+    return data
+
+
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
     # shows the artist page with the given artist_id
-    # TODO: replace with real artist data from the artist table, using artist_id
-    data = Artist.query.get_or_404(artist_id)
+    data = artist_serializer(Artist.query.get_or_404(artist_id))
     data1 = {
       "id": 4,
       "name": "Guns N Petals",
@@ -533,21 +557,19 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form = VenueForm()
-    venue={
-      "id": 1,
-      "name": "The Musical Hop",
-      "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-      "address": "1015 Folsom Street",
-      "city": "San Francisco",
-      "state": "CA",
-      "phone": "123-123-1234",
-      "website": "https://www.themusicalhop.com",
-      "facebook_link": "https://www.facebook.com/TheMusicalHop",
-      "seeking_talent": True,
-      "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-      "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get_or_404(venue_id)
+
+    form.name.data = venue.name
+    form.city.data = venue.city
+    form.genres.data = venue.genres
+    form.address.data = venue.address
+    form.state.data = venue.state
+    form.phone.data = venue.phone
+    form.website_link.data = venue.website_link
+    form.facebook_link.data = venue.facebook_link
+    form.seeking_talent.data = venue.seeking_talent
+    form.seeking_description.data = venue.seeking_description
+    form.image_link.data = venue.image_link
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
@@ -555,6 +577,42 @@ def edit_venue(venue_id):
 def edit_venue_submission(venue_id):
     # TODO: take values from the form submitted, and update existing
     # venue record with ID <venue_id> using the new attributes
+    form = VenueForm(request.form)
+    data = form.data.copy()
+    _ = data.pop('csrf_token')
+    venue_query = Venue.query.filter(Venue.id == venue_id)
+    if venue_query:
+        venue = venue_query.first()
+        venue_id = venue.id
+        genres = data.pop('genres')
+        error = False
+        try:
+            genres = convert_list_to_csv(genres)
+            # venue = Venue(**data, genres=genres)
+            venue.name = form.name.data
+            # =====================
+            # Todo: Start from here
+            # ======================
+            db.session.add(venue)
+            db.session.commit()
+            venue_id = venue.id
+        except Exception as e:
+            db.session.rollback()
+            error = True
+            print(e)
+        finally:
+            db.session.close()
+            if not error:
+                flash(
+                    f'Venue {form.name.data} was successfully listed!',
+                    category='success-message'
+                )
+                return redirect(url_for('show_venue', venue_id=venue_id))
+            else:
+                flash(
+                    f'An Error occurred while creating Venue {form.name.data}',
+                    category='error-message'
+                )
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 
@@ -606,7 +664,7 @@ def create_artist_submission():
 
 #  Shows
 #  ----------------------------------------------------------------
-def show_serializer(show, content_type=None):
+def show_serializer(show, content_type: db.Model = None):
     data = {
         'venue_id': show.venue_id,
         'venue_name': show.venue.name,
@@ -615,6 +673,15 @@ def show_serializer(show, content_type=None):
         'artist_image': show.artist.image_link,
         'start_time': show.start_time
     }
+    if isinstance(content_type, Artist):
+        data.pop('artist_id')
+        data.pop('artist_name')
+        data.pop('artist_image')
+        data['venue_image'] = show.venue.image_link
+
+    if isinstance(content_type, Venue):
+        data.pop('venue_id')
+        data.pop('venue_name')
 
     return data
     
